4a8c4529b2718f03bb9e698f1e48b0e7851a869e
WGitIJ <wisdomimade@paucasesnovescifp.cat>
2025-11-24 18:02:48 +0100

    Ask Jaume how does this has to work

--- src/main/java/com/paucasesnoves/xarxes/introduction/exercise3/Client.java
+++ src/main/java/com/paucasesnoves/xarxes/introduction/exercise3/Client.java
@@ -1,9 +1,11 @@
 package com.paucasesnoves.xarxes.introduction.exercise3;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -14,7 +16,7 @@ public class Client {
         try(Socket socket = new Socket("localhost", 2222)){
             System.out.println("Conectado al servidor en " + socket.getInetAddress().getHostAddress());
 
-            BufferedWriter writer = new BufferedWriter(new java.io.OutputStreamWriter(socket.getOutputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             System.out.print("Introduce el nombre de la calle: ");
             String street = scanner.nextLine();
             System.out.print("Introduce el código postal: ");
@@ -27,7 +29,11 @@ public class Client {
             Address address = new Address(street, zipCode, country, number);
 
             //Generate a JSON representation of the address object
+            ObjectMapper mapper = new ObjectMapper();
+
             File file = new File("address.json");
+            mapper.writerWithDefaultPrettyPrinter().writeValue(file,address);
+
 
         } catch (Exception e) {
             throw new RuntimeException(e);
